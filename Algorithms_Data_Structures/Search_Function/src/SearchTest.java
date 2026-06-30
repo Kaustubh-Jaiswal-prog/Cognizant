fe2b82b9fa44b3a28b60fcf44a4d58d020d5cf1d
Kaustubh-Jaiswal-prog <kaustubh.jaiswal_cs23@gla.ac.in>
2026-06-30 19:53:00 +0530

    Added Exercise 2 - Search Function

---
+++ Algorithms_Data_Structures/Search_Function/src/SearchTest.java
@@ -0,0 +1,38 @@
+public class SearchTest {
+
+    public static void main(String[] args) {
+
+        Product[] products = {
+
+                new Product(104, "Mouse", "Electronics"),
+                new Product(101, "Laptop", "Electronics"),
+                new Product(103, "Shoes", "Fashion"),
+                new Product(102, "Book", "Education"),
+                new Product(105, "Watch", "Accessories")
+
+        };
+
+        System.out.println("Linear Search");
+
+        Product result1 = SearchAlgorithms.linearSearch(products, 103);
+
+        if (result1 != null)
+            System.out.println(result1);
+        else
+            System.out.println("Product not found.");
+
+        System.out.println();
+
+        // Sort before Binary Search
+        SearchAlgorithms.sortProducts(products);
+
+        System.out.println("Binary Search");
+
+        Product result2 = SearchAlgorithms.binarySearch(products, 103);
+
+        if (result2 != null)
+            System.out.println(result2);
+        else
+            System.out.println("Product not found.");
+    }
+}
